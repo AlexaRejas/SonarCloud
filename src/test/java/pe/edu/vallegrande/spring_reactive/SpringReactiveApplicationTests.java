f5ab925bee45d630493a4c84ae90d725b0536627
Alexa Rejas Carrasco <126887187+AlexaRejas@users.noreply.github.com>
2024-10-21 02:03:22 -0500

    Update SpringReactiveApplicationTests.java

--- src/test/java/pe/edu/vallegrande/spring_reactive/SpringReactiveApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/spring_reactive/SpringReactiveApplicationTests.java
@@ -1,13 +1,11 @@
 package pe.edu.vallegrande.spring_reactive;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class SpringReactiveApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootApplication
+public class SpringReactiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveApplication.class, args);
+    }
 }
